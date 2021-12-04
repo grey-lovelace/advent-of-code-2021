eb5c09d3f6b75bff0b725faeac3be980f6724b17
grey-lovelace <austin.grey.lovelace@gmail.com>
2021-12-04 11:39:42 -0600

    Refactor day4 for new approach

--- day04/main.kts
+++ day04/main.kts
@@ -4,34 +4,27 @@ check(1924 == part2("sample.txt"))
 println("Part2 = ${part2("input.txt")}")
 
 fun part1(fileName: String) : Int {
-    val input = java.io.File(fileName).readText().split(Regex("(\r?\n){2}"))
-    val calls = input[0].split(",").map{ it.toInt() }
-    val cards = (1..input.lastIndex).map{ Card(input[it]) }
-    calls.forEach{ call -> cards.forEach{ if(it.callNum(call)) return it.score() * call }}
-    return 0
+    return BingoGame(fileName).cards.minByOrNull{ it.winningRound }!!.score()
 }
 
 fun part2(fileName: String) : Int {
-    val input = java.io.File(fileName).readText().split(Regex("(\r?\n){2}"))
-    val calls = input[0].split(",").map{ it.toInt() }
-    var cards = (1..input.lastIndex).map{ Card(input[it]) }
-    calls.forEach{ call ->
-        cards.forEach{ if(it.callNum(call) && cards.size == 1) return it.score() * call }
-        cards = cards.filter{ !it.hasBingo() }
-    }
-    return 0
+    return BingoGame(fileName).cards.maxByOrNull{ it.winningRound }!!.score()
 }
 
-class Card(rawString : String) {
-    var rows: List<List<Int>> = rawString.split(Regex("\r?\n")).map{
-            line -> line.trim().split(Regex("\\s+")).map{ num -> num.toInt() }
-    }
-    var columns: List<List<Int>> = rows.indices.map{ i -> rows.map{ it[i] } }
-    fun hasBingo() : Boolean { return this.rows.any{ it.isEmpty() } || this.columns.any{ it.isEmpty() } }
-    fun score() : Int { return this.rows.map{ it.sum() }.sum() }
-    fun callNum(call: Int) : Boolean {
-        this.rows = this.rows.map{ row -> row.filter{ it!=call }}
-        this.columns = this.columns.map{ col -> col.filter{ it!=call }}
-        return this.hasBingo()
-    }
+class BingoGame(fileName : String) {
+    val input = java.io.File(fileName).readText().split(Regex("(\r?\n){2}"))
+    val calls = input[0].split(",").map{ it.toInt() }
+    val cards = (1..input.lastIndex).map{ Card(input[it], calls) }
+}
+
+class Card(rawString : String, calls: List<Int>) {
+    var rowsRaw = rawString.lines().map{ line -> line.trim().split(Regex("\\s+")).map{ it.toInt() } }
+    // Rows and columns hold the round the number is called. rowsRaw holds the real numbers.
+    var rows = this.rowsRaw.map{ row -> row.map{ num -> calls.indexOf(num) } }
+    var columns = this.rows.indices.map{ i -> rows.map{ it[i] } }
+    var winningRound = minOf(rows.minOf{ it.maxOrNull()!! },columns.minOf{ it.maxOrNull()!! })
+    var winningCall = calls[this.winningRound]
+    fun score() : Int = this.winningCall * this.rows.indices.map{ i ->
+        this.rows[i].indices.map{ if(this.rows[i][it] > this.winningRound) this.rowsRaw[i][it] else 0 }.sum()
+    }.sum()
 }
