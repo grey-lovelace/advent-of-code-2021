aec457676df859b54c7cd302d4ebff23f73f0295
grey <austin.grey.lovelace@gmail.com>
2021-12-07 12:52:32 -0600

    improve day 7 speed

--- day07/main.kts
+++ day07/main.kts
@@ -5,12 +5,14 @@ println("Part2 = ${part2(java.io.File("input.txt"))}")
 
 fun part1(file: java.io.File) : Int {
     val input = file.readText().split(',').map{ it.toInt() }
-    return (input.minOrNull()!!..input.maxOrNull()!!)
-        .map{ pos -> input.map{ kotlin.math.abs(it-pos) }.sum() }.minOrNull()!!
+    return (input.minOrNull()!!..input.maxOrNull()!!).map{ pos ->
+            input.map{ kotlin.math.abs(it-pos) }.sum()
+        }.minOrNull()!!
 }
 
 fun part2(file: java.io.File) : Int {
     val input = file.readText().split(',').map{ it.toInt() }
-    return (input.minOrNull()!!..input.maxOrNull()!!)
-        .map{ pos -> input.map{ (0..kotlin.math.abs(it-pos)).sum() }.sum() }.minOrNull()!!
+    return (input.minOrNull()!!..input.maxOrNull()!!).map{ pos ->
+            input.map{ kotlin.math.abs(it-pos ) }.map{ it*(it+1)/2 }.sum()
+        }.minOrNull()!!
 }
