0360575f760df41312f515b33852b2fd698fa0af
grey <austin.grey.lovelace@gmail.com>
2021-12-06 23:39:01 -0600

    day 7 first pass

---
+++ day07/main.kts
@@ -0,0 +1,16 @@
+check(37 == part1(java.io.File("sample.txt")))
+println("Part1 = ${part1(java.io.File("input.txt"))}")
+check(168 == part2(java.io.File("sample.txt")))
+println("Part2 = ${part2(java.io.File("input.txt"))}")
+
+fun part1(file: java.io.File) : Int {
+    val input = file.readText().split(',').map{ it.toInt() }
+    return (input.minOrNull()!!..input.maxOrNull()!!)
+        .map{ pos -> input.map{ kotlin.math.abs(it-pos) }.sum() }.minOrNull()!!
+}
+
+fun part2(file: java.io.File) : Int {
+    val input = file.readText().split(',').map{ it.toInt() }
+    return (input.minOrNull()!!..input.maxOrNull()!!)
+        .map{ pos -> input.map{ (0..kotlin.math.abs(it-pos)).sum() }.sum() }.minOrNull()!!
+}
